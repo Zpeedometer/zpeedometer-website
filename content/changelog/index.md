bc38a25445c598abbf583852fbae3043396733a4
Alexander499 <aginfo9@gruenefeld.de>
2025-12-19 22:14:15 +0100

    Refactor Navigation Scrolling Logic and more

--- content/changelog/index.md
+++ content/changelog/index.md
@@ -5,8 +5,8 @@ type  = "changelog"
 
 <time datetime="2023-07-06">6 Jul 2023</time>
 ## 1.0.1
-- Several bug fixes
+- Example
 ---
 <time datetime="2023-07-06">6 Jul 2023</time>
 ## 1.0.0
-- First public release
+- Example
--- static/script.js
+++ static/script.js
@@ -1,23 +1,9 @@
 // Sections
 const navListItems = document.querySelectorAll("nav li:not(:last-of-type)");
-const sections = ["features", "faq"];
-
-const scrollToSection = (id) => {
-  const target = document.getElementById(id);
-  if (!target) {
-    window.location.href = `${location.protocol}//${location.host}/#${id}`;
-  } else window.scrollTo({ top: id === "home" ? 0 : target.offsetTop - 100, behavior: "smooth" });
-};
 
 navListItems.forEach(item => item.onclick = () => {
   const sectionId = item.id.replace("Link", "");
-  if (sections.includes(sectionId)) scrollToSection(sectionId);
-  history.pushState(null, "", `#${sectionId}`);
-});
-
-window.addEventListener("load", () => {
-  const sectionId = location.hash.slice(1);
-  if (sections.includes(sectionId)) scrollToSection(sectionId);
+  if (["features", "faq"].includes(sectionId) && document.getElementById(sectionId)) window.scrollTo({ top: sectionId === "home" ? 0 : document.getElementById(sectionId).offsetTop - 100, behavior: "smooth" });
 });
 
 // Icons

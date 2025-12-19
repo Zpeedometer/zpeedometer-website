7ac8342e078092c4a7d23f672de37e89be2afc38
Alexander499 <aginfo9@gruenefeld.de>
2025-12-19 22:29:56 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,6 +3,7 @@ const navListItems = document.querySelectorAll("nav li:not(:last-of-type)");
 
 navListItems.forEach(item => item.onclick = () => {
   const sectionId = item.id.replace("Link", "");
+  if (!(window.location.pathname === "/")) window.location.href = "/";
   if (["features", "faq"].includes(sectionId) && document.getElementById(sectionId)) window.scrollTo({ top: sectionId === "home" ? 0 : document.getElementById(sectionId).offsetTop - 100, behavior: "smooth" });
 });
 
